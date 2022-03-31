a9aebae226fc1d5934ac12306ae83f23402cfeb0
denizatuk <dnz.atk96@gmail.com>
2022-04-01 02:25:53 +0300

    Whyarrays

---
+++ WhyArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhyArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studends = { "Deniz", "Ahmet", "Doğa" };
+            foreach (var student in studends)
+            {
+                Console.WriteLine(student);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
