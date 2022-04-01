8985faa808a2444d9eac7e71e1eb88e7373944c7
denizatuk <dnz.atk96@gmail.com>
2022-04-01 03:01:46 +0300

    odev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        static String Message = "Müşteri Kayıt Edildi.";
+        
+
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(Message + " " + musteri.Name);
+        }
+
+
+        internal void Listele(ref Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Name);
+            }
+        }
+    }
+}
