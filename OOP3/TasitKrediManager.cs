90b15e4d027fe647aad1c79fe7c4788d00fe4d4e
denizatuk <dnz.atk96@gmail.com>
2022-04-15 00:18:05 +0300

    oop3

---
+++ OOP3/TasitKrediManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class TasitKrediManager : IKrediManager
+    {
+        public void CıktıAl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("Taşıt kıredisi Hesaplandı");
+        }
+    }
+}
